47999e9c05d0fd4db71daa17e7731c23d0c95cca
Matias Escobar <mescobar@chicagobooth.edu>
2023-05-25 19:08:11 +0000

    Nav bar working, added first/last name to user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_25_142645) do
+ActiveRecord::Schema.define(version: 2023_05_25_150336) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2023_05_25_142645) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
