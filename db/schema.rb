2e9e9063aed1cca0919962d1daeb9cae729327dc
Matias Escobar <mescobar@chicagobooth.edu>
2023-05-25 00:18:33 +0000

    Messages table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_05_25_001614) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.string "role"
+    t.integer "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
